b3a9269d37e0f9044662a0c9cf4abbd865c983d3
prymalchaos <114586585+prymalchaos@users.noreply.github.com>
2025-12-22 14:25:06 +1100

    Update phases.js

--- js/phases/phases.js
+++ js/phases/phases.js
@@ -1,8 +1,8 @@
-// /js/phases/phases.js
-// Central registry for all phase modules
-
+// ./js/phases/phases.js
 import { phase1 } from "./phase1.js";
 
 export const PHASE_MODULES = {
   1: phase1
+  // 2: phase2,
+  // 3: phase3,
 };
