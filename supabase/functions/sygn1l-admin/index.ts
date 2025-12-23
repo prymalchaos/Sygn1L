46d9008178dd1f64649e304d73c52f6ad407b58f
prymalchaos <114586585+prymalchaos@users.noreply.github.com>
2025-12-23 23:03:17 +1100

    Update index.ts

--- supabase/functions/sygn1l-admin/index.ts
+++ supabase/functions/sygn1l-admin/index.ts
@@ -1,3 +1,5 @@
+// test
+
 // supabase/functions/sygn1l-admin/index.ts
 import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
